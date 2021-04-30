794b514d654fe88b6d4c5619c7cb0c6995d96f4b
Paul Rogalinski-Pinter <paul@paul.vc>
2021-04-30 17:29:08 +0200

    cleaned up code, improved ux

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,12 @@ if (program.args.length < 1) program.help();
 const websocketURL = program.args[0]
 const jsonRPCClient = JsonRpcWs.createClient();
 
-const bscreen = blessed.screen();
+const bscreen = blessed.screen({
+  smartCSR: true,
+  terminal: 'xterm-256color',
+  fullUnicode: true
+});
+
 const body = blessed.box({
   top: 0,
   left: 0,
@@ -39,13 +44,14 @@ const inputBar = blessed.textbox({
   mouse: true,
   inputOnFocus: true,
   style: {
-    fg: 'white',
-    bg: 'blue'	// Blue background so you see this is different from body
+    fg: '#00ff00',
+    bg: '#002000'	// Blue background so you see this is different from body
   }
 });
 // Add body to blessed screen
 bscreen.append(body);
 bscreen.append(inputBar);
+inputBar.focus();
 
 // Close the example on Escape, Q, or Ctrl+C
 bscreen.key(['escape', 'q', 'C-c'], (ch, key) => (process.exit(0)));
@@ -62,38 +68,38 @@ const err = (text?: any, ...optionalParams: any[]) => {
   bscreen.render();
 }
 
-inputBar.focus();
+const jsonRPCResponseHandler = (error: any, reply: String) => {
+  printLine(error, "" + reply, "<");
+}
 
-const jsonRPCResponseHandler = (error: any, reply: any) => {
-  if (error) {
-    err(get(error, 'message'));
+function formatLine(e: String, prompt: String) {
+  return `{#505050-fg}${new Date().toUTCString()}{/} ${prompt} ${e}`;
+}
+
+function printLine(isError: boolean, e: String, prompt: String) {
+  if (isError) {
+    err(formatLine(e,prompt));
   } else {
-    log('<', reply);
+    log(formatLine(e,prompt));
   }
 }
+
 jsonRPCClient.connect(websocketURL, function connected() {
   if (jsonRPCClient.isConnected()) {
-    log("Connected to ", websocketURL);
+    printLine(false, `Connected to ${websocketURL}`,"<");
+    printLine(false, `/quit and /exits ltes the application`,"<");
     jsonRPCClient.send("printer.gcode.script", {"script": "STATUS"}, jsonRPCResponseHandler);
 
     jsonRPCClient.expose("notify_gcode_response", function (data: []) {
       data.forEach((e) => {
-        let date_ob = new Date();
-        let hours = date_ob.getHours();
-        let minutes = date_ob.getMinutes();
-        let seconds = date_ob.getSeconds();
-        if (String(data).startsWith('!!')) {
-          err(hours + ":" + minutes + ":" + seconds + "<", e)
-        } else {
-          log(hours + ":" + minutes + ":" + seconds + "<", e)
-        }
+        printLine(String(data).startsWith('!!'), e, "<");
       })
     })
 
     inputBar.on('submit', (text) => {
       if (text === "/quit" || text === "/exit") process.exit(1);
       jsonRPCClient.send("printer.gcode.script", {"script": text}, jsonRPCResponseHandler);
-      log('>', text);
+      printLine(false,text, "{green-fg}>");
       inputBar.clearValue();
       inputBar.focus();
     });
